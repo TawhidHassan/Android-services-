4b4a31bae0f4fd84037fbdbb4d5c4fc717b4f021
sifat007 <sifat27.ht@gmail.com>
2021-02-27 19:13:36 +0600

    make the design

--- app/src/main/java/com/example/androidservicesworking/MainActivity.java
+++ app/src/main/java/com/example/androidservicesworking/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.androidservicesworking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyTag";
+    public static final String MESSAGE_KEY = "message_key";
+    private ScrollView mScroll;
+    private TextView mLog;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
+
+
+
+    private void initViews() {
+        mScroll = (ScrollView) findViewById(R.id.scrollLog);
+        mLog = (TextView) findViewById(R.id.tvLog);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+    }
+
+    public void clearOutput(View v) {
+
+
+    }
+
+    public void runCode(View v) {
+
+    }
+
 }
+
