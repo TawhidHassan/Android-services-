268944a8c9e24d024c0951237702d4d0bdaa5265
sifat007 <sifat27.ht@gmail.com>
2021-02-28 17:25:49 +0600

    dwonload handelar

---
+++ app/src/main/java/com/example/androidservicesworking/DownloadHandler.java
@@ -0,0 +1,31 @@
+package com.example.androidservicesworking;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class DownloadHandler extends Handler {
+
+    private static final String TAG = "MyTag";
+
+    public DownloadHandler() {
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+
+        downloadSong(msg.obj.toString());
+    }
+
+    private void downloadSong(final String songName){
+        Log.d(TAG, "run: staring download");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "downloadSong: "+songName+" Downloaded...");
+    }
+
+}
--- app/src/main/java/com/example/androidservicesworking/services/MyDwonloadService.java
+++ app/src/main/java/com/example/androidservicesworking/services/MyDwonloadService.java
@@ -3,28 +3,44 @@ package com.example.androidservicesworking.services;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 
+import com.example.androidservicesworking.DownloadThread;
 import com.example.androidservicesworking.MainActivity;
 
 public class MyDwonloadService extends Service {
     private static final String TAG ="MyTag" ;
-
+    private DownloadThread mDownlaodThread;
     public MyDwonloadService() {
     }
 
 
     @Override
     public void onCreate() {
-        super.onCreate();
-        Log.d(TAG,"onCreate Call");
+        Log.d(TAG, "onCreate: called");
+
+        mDownlaodThread=new DownloadThread();
+        mDownlaodThread.start();
+
+        while (mDownlaodThread.mHandler == null){
+
+        }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG,"onStartCommand Call");
-        String songName=intent.getStringExtra(MainActivity.MESSAGE_KEY);
-        downloadSong(songName);
+
+        Log.d(TAG, "onStartCommand: called: Song Name: "+intent.getStringExtra(MainActivity.MESSAGE_KEY));
+        Log.d(TAG, "onStartCommand: called: Intent Id: "+startId);
+        final String songName=intent.getStringExtra(MainActivity.MESSAGE_KEY);
+
+        Message message=Message.obtain();
+        message.obj=songName;
+        message.arg1=startId;
+        mDownlaodThread.mHandler.sendMessage(message);
+
+
         return Service.START_REDELIVER_INTENT;
     }
 
